9525505b2ef9d1324120a78f27811cbdf3fe4899
AngelDown12 <angeldown12xz@gmail.com>
2025-08-11 15:53:43 -0600

    Crear antiprivado.js

---
+++ plugins/antiprivado.js
@@ -0,0 +1,39 @@
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+  if (m.isBaileys && m.fromMe) return true;
+  if (m.isGroup) return false;
+  if (!m.message) return true;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return true;
+
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    const mensaje = `𝙃𝙤𝙡𝙖 👋, 𝙎𝙤𝙮 𝘼𝙡𝙚𝙚𝘽𝙤𝙩 🤖!
+•𝙀𝙨𝙩𝙖 𝙋𝙧𝙤𝙝𝙞𝙗𝙞𝙙𝙤 𝙀𝙨𝙘𝙧𝙞𝙗𝙞𝙧𝙢𝙚 𝘼𝙡 𝙋𝙧𝙞𝙫𝙖𝙙𝙤 🚫.
+𝙎𝙞 𝘿𝙚𝙨𝙚𝙖𝙨 𝘼𝙙𝙦𝙪𝙞𝙧𝙞𝙧 𝘼𝙡𝙚𝙚𝘽𝙤𝙩 🤖, 𝙈𝙚𝙣𝙨𝙪𝙖𝙡𝙢𝙚𝙣𝙩𝙚 𝙤 𝙥𝙚𝙧𝙢𝙖𝙣𝙚𝙣𝙩𝙚𝙢𝙚𝙣𝙩𝙚 𝙡𝙤 𝙥𝙪𝙚𝙙𝙚𝙨 𝙝𝙖𝙘𝙚𝙧𝙡𝙤 𝙖𝙡 ✅: 
+
+https://wa.me/573206095607?text=.Adquirir
+
+𝙏𝙖𝙢𝙗𝙞𝙚𝙣 𝙨𝙚 𝙝𝙖𝙘𝙚𝙣 𝙗𝙤𝙩𝙨 𝙥𝙚𝙧𝙨𝙤𝙣𝙖𝙡𝙞𝙯𝙖𝙙𝙤𝙨 🤖🖌️!`;
+
+    await conn.sendMessage(m.chat, {
+      text: mensaje,
+      contextInfo: {
+        externalAdReply: {
+          title: '𝐁𝐨𝐭 𝐁𝐲 𝐀𝐥𝐞𝐞 👑',
+          body: '𝐁𝐨𝐭 𝐁𝐲 𝐀𝐥𝐞𝐞 👑',
+          thumbnailUrl: 'https://qu.ax/FxpUy.jpg',
+          sourceUrl: '',
+          mediaType: 1,
+          renderLargerThumbnail: false,
+          showAdAttribution: false
+        }
+      }
+    });
+
+    await this.updateBlockStatus(m.chat, 'block');
+    console.log(`Usuario ${m.sender} bloqueado por contacto privado.`);
+    return true;
+  }
+  return false;
+}

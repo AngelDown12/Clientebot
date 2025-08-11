f95828c75c31478db175200f08747e83cbb962bf
AngelDown12 <angeldown12xz@gmail.com>
2025-08-11 15:30:09 -0600

    Actualizar antilink.js

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -4,7 +4,7 @@
 const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
 
 export async function before(m, { conn, isAdmin, isBotAdmin }) {
-    if (m.isBaileys) return !0; // Ignorar mensajes internos (incluidos los del bot)
+    if (m.isBaileys) return !0;
     if (!m.isGroup) return !1;
 
     let chat = global.db.data.chats[m.chat];
@@ -12,16 +12,14 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
     const isGroupLink = linkRegex.exec(m.text);
 
     if (chat.antiLink && isGroupLink && !isAdmin) {
-        // 🚫 Evitar detectar links enviados por el mismo bot
+ 
         if (m.sender === conn.user.jid) return !0;
 
-        // 🛡️ Excepción: permitir link del grupo actual
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`;
             if (m.text.includes(linkThisGroup)) return !0;
         }
 
-        // ⚠️ Notificar y eliminar mensaje / expulsar si posible
         await conn.reply(m.chat, `*❌ _Un Enlace Detectado_*\n\nNo permitimos enlaces de otros grupos, por eso *@${m.sender.split('@')[0]}* será expulsado.${isBotAdmin ? '' : '\n\n⚠️ No soy admin así que no puedo expulsar :('}`, null, {
             mentions: [m.sender],
         });

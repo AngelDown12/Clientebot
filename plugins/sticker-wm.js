ba0ccb1c88ba6e298c9db3510ca88db7e28f6e28
AngelDown12 <angeldown12xz@gmail.com>
2025-08-13 01:59:16 -0600

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,21 +1,21 @@
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
-  if (!m.quoted) return m.reply(`⭐ Responde al Sticker.`)
+  if (!m.quoted) return m.reply(`𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐀𝐥 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 🌴`)
   let stiker = false
   try {
     let [packname, ...author] = text.split('|')
     author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) return m.reply(`⭐ Responde al Sticker.`)
+    if (!/webp/.test(mime)) return m.reply(`𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐀𝐥 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 🌴`)
     let img = await m.quoted.download()
-    if (!img) return m.reply(`⭐ Responde al Sticker.`)
+    if (!img) return m.reply(`𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐀𝐥 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 🌴`)
     stiker = await addExif(img, packname || '', author || '')
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m)
-    else return m.reply(`⭐ Responde al Sticker.`)
+    else return m.reply(`𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐀𝐥 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 🌴`)
   }
 }
 handler.help = ['wm <nombre>|<autor>']

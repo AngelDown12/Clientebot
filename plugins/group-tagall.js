f94d989261b0a56d424054bcb0ca9c23e68b01e0
AngelDown12 <angeldown12xz@gmail.com>
2025-08-15 00:54:38 -0600

    Crear group-tagall.js

---
+++ plugins/group-tagall.js
@@ -0,0 +1,25 @@
+const handler = async (m, { conn, participants, isAdmin, isOwner, command }) => {
+  if (!m.isGroup) return;
+  if (!isAdmin && !isOwner) return global.dfail?.('admin', m, conn);
+
+  const total = participants.length;
+  let texto = `*!  MENCION GENERAL  !*\n`;
+  texto += `*PARA ${total} MIEMBROS* 🗣️\n\n`;
+
+  for (const user of participants) {
+    const numero = user.id.split('@')[0];
+    texto += `🗣️ @${numero}\n`;
+  }
+
+  await conn.sendMessage(m.chat, {
+    text: texto,
+    mentions: participants.map(p => p.id)
+  });
+};
+
+handler.customPrefix = /^\.?(todos|invocar|invocacion|invocación)$/i;
+handler.command = new RegExp(); // lo puedes dejar así
+handler.group = true;
+handler.admin = true;
+
+export default handler;
